d39539a0152c3e680cb98c12a0d32a1a97b7a2df
Phodal HUANG <h@phodal.com>
2018-06-25 08:06:55 +0800

    [workflow] add test

--- src/workflow/workflow.ts
+++ src/workflow/workflow.ts
@@ -1,16 +1,19 @@
-let data = {
-  transact: {
-    name: 'transact',
-    status: 'success'
-  },
-  approve: {
-    name: 'approve',
-    status: 'processing'
-  },
-  bank: {
-    name: 'approve',
-    status: 'todo'
-  }
+export interface IWorkflow {
+  name: string
+  status: string
 }
 
-export function workflowParser() {}
+export function workflowParser(data: any) {
+  const keys = Object.keys(data)
+  const results = []
+  for (let key of keys) {
+    let process = data[key] as IWorkflow
+    results.push({
+      name: process.name,
+      status: process.status,
+      icon: `icon-${process.status}`
+    })
+  }
+
+  return results
+}
--- test/oadsl.test.ts
+++ test/oadsl.test.ts
@@ -1,5 +1,6 @@
 import SQL from '../src/chain/chain'
-import { FormValidator, RegexMap, VALIDATE_ERROR } from '../src/validate/validate'
+import { FormValidator, RegexMap } from '../src/validate/validate'
+import { workflowParser } from '../src/workflow/workflow'
 
 describe('OA DSL Test', () => {
   it('Where defined', () => {
@@ -10,6 +11,30 @@ describe('OA DSL Test', () => {
   it('Where defined', () => {
     let sql = new SQL()
     expect(sql.WHERE().ORDERBY()).toBeDefined()
+  })
+
+  it('Workflow Test', () => {
+    let data = {
+      transact: {
+        name: 'Transact',
+        status: 'success'
+      },
+      approve: {
+        name: 'Approve',
+        status: 'processing'
+      },
+      bank: {
+        name: 'Bank',
+        status: 'todo'
+      }
+    }
+
+    let results = workflowParser(data)
+    expect(results).toEqual([
+      { icon: 'icon-success', name: 'Transact', status: 'success' },
+      { icon: 'icon-processing', name: 'Approve', status: 'processing' },
+      { icon: 'icon-todo', name: 'Bank', status: 'todo' }
+    ])
   })
 
   it('form validate', () => {
